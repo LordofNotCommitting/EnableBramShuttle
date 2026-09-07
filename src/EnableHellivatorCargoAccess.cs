6b2e06efc507d82aab19c35db14e1ba0ce393c95
LordofNotCommitting <shadymcshade247@gmail.com>
2026-09-07 12:11:04 -0400

    first commit

---
+++ src/EnableHellivatorCargoAccess.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EnableBramShuttle
+{
+    [HarmonyPatch(typeof(ElevatorWindow), nameof(ElevatorWindow.Configure))]
+    public static class EnableHellivatorCargoAccess
+    {
+        public static void Postfix(ElevatorWindow __instance)
+        {
+            //Plugin.Logger.Log("--- main menu awake");
+
+            if (__instance._shuttleCargoButton != null)
+            {
+                ShuttleCargoDepartment department = __instance._magnumSpaceship.GetDepartment<ShuttleCargoDepartment>();
+                __instance._shuttleCargoButton.gameObject.SetActive(department != null && department.IsActiveDepartment());
+            }
+
+        }
+    }
+}
